226c9172838b086af415266bd930aaac7aa40bc3
tubbadu <tubbadu@gmail.com>
2023-07-19 15:21:19 +0200

    backup upload

--- contents/code/main.js
+++ contents/code/main.js
@@ -1,6 +1,7 @@
-// journalctl -g "js:" -f 
-
-
+function log(t){
+    const DEBUG=false;
+    if(DEBUG) console.warn("debug: " + t)
+}
 function setTimeout(func, time){
     var timer = new QTimer();
     timer.interval = time; // set the time in milliseconds
@@ -8,91 +9,35 @@ function setTimeout(func, time){
     timer.timeout.connect(this, func);
     timer.start();
 }
-  
-  
-  //workspace.slotWindowQuickTileRight()
-  
-  workspace.clientAdded.connect(client => {
-  
-      workspace.activeClient = client
-      var appName = client.resourceName
-      var app = client.geometry // x, y, height, width
-      var freeWidth = workspace.clientArea("PlacementArea", workspace.activeScreen, 0).width
-      var freeHeight = workspace.clientArea("PlacementArea", workspace.activeScreen, 0).height
-      var freeX = workspace.clientArea("PlacementArea", workspace.activeScreen, 0).x
-      var freeY = workspace.clientArea("PlacementArea", workspace.activeScreen, 0).y
-  
-      //print(appName + ": " + app.x + ", " + app.y + ", " + app.height + ", " + app.width + ", " + freeX + ", " + freeY + ", " + freeHeight + ", " + freeWidth)
-      if(app.height == freeHeight && (Math.abs((freeWidth/2) - app.width) < 1.1)){ // is half maximized vertically
-        //maximize it well
-        //check if left or right or nothing
-        if(Math.abs(app.x-freeX) < 1.1){ // if left
-          print("left")
-          workspace.activeClient = client
-          setTimeout(function () {
-              workspace.slotWindowQuickTileLeft()
-          }, 1);
-        } else if(Math.abs(freeWidth/2 + freeX - app.x) < 1.1){ // if right
-          print("right")
-          workspace.activeClient = client
-          setTimeout(function () {
-              workspace.slotWindowQuickTileRight()
-          }, 1);
-        } else {
-          print("nothing")
-  
-        }
-      } else {
-        print("nothing to do")
-      }
-  });
-  
-  /**/
-  
-  /*
-  function log(txt){
-    console.log(txt)
-  }
-  
-  
-  function dewit2() {
-      var appName = "telegram" // to be changed
-      var appClass = "telegramdesktop" // to be changed
-      var found = false
-      var clients = workspace.clientList();
-      for (var i=0; i<clients.length; i++){
-        var client = clients[i];
-        if (client.moveable) { //change or in and
-          //log(client);
-          //log(client.resourceName);
-          //log(client.resourceClass);
-          //toggle it
-          if(client.resourceName == appName && client.resourceClass == appClass){
-            found = true
-            if(workspace.activeClient == client){
-              // minimize it
-              client.minimized = true;
-            }
-            else{
-              // raise it
-              workspace.activeClient = client;
-            }
-          }
-        }
-      }
-      if(!found){
-        log("running it")
-        //run it
-        //callDBus('org.kde.klauncher5', '/KLauncher', 'exec_blind', '/usr/bin/bash', '/home/tubbadu/.bin/telegram');
-        //callDBus('org.kde.klauncher5', '/KLauncher', 'exec_blind', '/usr/bin/bash', 'konsole');
-        callDBus("org.kde.krunner","/org/kde/krunner","org.kde.KDBusService","CommandLine", "konsole", "usr/bin/bash");
-      }
-  }
-  
-  dewit2()
-  registerShortcut("toggleTelegram", "do magic thing", "Meta+K", dewit2);
-  
-  */
-  
-  
-  
+
+
+function autoTile(client){
+    const geom = JSON.stringify([client.x, client.y, client.height, client.width]); // perhaps it's better to use client.geometry, that gives a QRectF
+    const ret = {
+        "[960,0,1080,960]": function() {workspace.slotWindowQuickTileRight()}, // right
+        "[40,0,1080,920]": function() {workspace.slotWindowQuickTileLeft()}, // left
+        "[40,540,540,1880]": function() {workspace.slotWindowQuickTileBottom()}, // down
+        "[40,0,540,1880]": function() {workspace.slotWindowQuickTileTop()}, // up
+        "[960,0,540,960]": function() {workspace.slotWindowQuickTileTopRight()}, // up-right
+        "[40,0,540,920]": function() {workspace.slotWindowQuickTileTopLeft()}, // up-left
+        "[40,540,540,920]": function() {workspace.slotWindowQuickTileBottomLeft()}, // down-left
+        "[960,540,540,960]": function() {workspace.slotWindowQuickTileBottomRight()} // down-right
+    }
+    log("is ok" + ret[geom])
+    if(ret[geom] === undefined){
+        log("not tiling")
+    } else {
+        log("tiling")
+        ret[geom]();
+    }
+}
+
+function onClientAdded(client){
+    log("tiling started:")
+    setTimeout(function() {
+        autoTile(client);
+        log("done")
+    }, 100)
+}
+
+workspace.clientAdded.connect(client => onClientAdded(client))
